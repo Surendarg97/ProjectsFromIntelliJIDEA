831a0108eb82279bb21181f903025857b6e9c957
surendar <surendarg97@gmail.com>
2020-07-10 09:19:51 +0530

    IntelliJIDEA codes commit

---
+++ AutoboxingAndUnboxing/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Bank bank= new Bank("SBI");
+
+        bank.addCustomer("salem","surendar",45.23);
+        bank.addCustomer("chennai","surendar",45.23);
+        bank.addCustomer("salem","surendar",45.23);
+
+    }
+}
